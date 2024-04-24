ee79c270e5c606c878b4910acb8214c11a2d7388
Mehrdad Moradi <61975123+rfarrokhi@users.noreply.github.com>
2024-04-23 23:19:31 -0500

    Modified project's folder structure

--- server/Courses/model.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-import courseSchema from "./schema.js";
-
-const courseModel = mongoose.model("Courses", courseSchema);
-export default courseModel;
--- server/Modules/model.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-import moduleSchema from "./schema.js";
-
-const moduleModel = mongoose.model("Modules", moduleSchema);
-export default moduleModel;
